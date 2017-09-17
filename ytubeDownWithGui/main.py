64d3a74c6768a3f0c771dd2da463b29281b9972d
dopedev <dahiya.surya12@gmail.com>
2017-09-18 00:10:51 +0545

    first commit

---
+++ ytubeDownWithGui/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import urllib.request
+from bs4 import BeautifulSoup
+from pytube import YouTube
+ 
+class download():
+    def start_download(self,url,video_type,place):
+        url_open = urllib.request.urlopen(url)
+        url_open = url_open.read(50000)
+        soup = BeautifulSoup(url_open,"html.parser")
+        title = soup.html.head.title.text
+        title = title.replace(" - YouTube","")
+        yt = YouTube(url)
+        yt.set_filename(title)
+        video = yt.get('mp4', video_type)
+        video.download(place)
